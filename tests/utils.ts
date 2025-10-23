5d3c752b5e550aa50b4ee0fa5df546ea5c99cba9
Tomasz Kordiak <dotomika@gmail.com>
2025-10-23 21:33:58 +0200

    separate tests

--- tests/utils.ts
+++
@@ -1,4 +0,0 @@
-export function windowLocationMock(locationObject: { [key: string]: any }): void {
-  global.window = Object.create(window);
-  global.window.location = locationObject as string & Location;
-}
